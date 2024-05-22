5332de0daaaf956c15147041a3e7912f27045146
webapplicationuday <uday.ss048@gmail.com>
2024-05-22 16:53:33 +0000

    solution

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "covid19India.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    stateId: dbObject.state_id,
+    stateName: dbObject.state_name,
+    population: dbObject.population,
+  };
+};
+
+app.get("/states/", async (request, response) => {
+  const getStatesQuery = `
+ SELECT
+ *
+ FROM
+ state;`;
+  const statesArray = await db.all(getStatesQuery);
+  response.send(
+    statesArray.map((eachState) => convertDbObjectToResponseObject(eachState))
+  );
+});
